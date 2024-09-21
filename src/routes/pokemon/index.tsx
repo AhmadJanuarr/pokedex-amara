d84da988c53d9b3fbd25a7004eba7f8365b49e96
MAD <ahmadjanuaramri2015@gmail.com>
2024-09-22 02:21:15 +0700

    refactor : remove unused code and fix the lines

--- src/routes/pokemon/index.tsx
+++ src/routes/pokemon/index.tsx
@@ -1,70 +1,93 @@
-import { createFileRoute, useNavigate } from '@tanstack/react-router'
-import { Field, FieldProps, Form, Formik } from "formik";
-import { Container, Input, FormControl, Button, FormErrorMessage, Center, Flex, Box } from "@chakra-ui/react";
+import { createFileRoute } from "@tanstack/react-router";
+import {
+  Container,
+  Box,
+  Flex,
+  Heading,
+  Spinner,
+  Center,
+  Popover,
+  PopoverTrigger,
+  Button,
+  PopoverContent,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverHeader,
+  PopoverBody,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { SearchForm } from "../../components/SearchForm";
+import { PokemonCard } from "../../components/PokemonCard";
+import { FiBookOpen } from "react-icons/fi";
+import { Icon } from "@chakra-ui/react";
+import { usePokemon } from "../../api/pokemon";
 
-export const Route = createFileRoute('/pokemon/')({
-  component: Pokemon
-})
+export const Route = createFileRoute("/pokemon/")({
+  component: Pokemon,
+});
 
+const variants = {
+  open: { y: "-350%", transition: { duration: 0.4 } },
+  closed: { y: 0 },
+};
 
 function Pokemon() {
-  const navigate = useNavigate()
-  function validateSearch(value: string) {
-    let error;
-    if (value === "") {
-      error = "This field is required";
-    }
-    return error;
-  }
+  const [isOpen, setIsOpen] = useState(false);
 
-  interface MyFormValues {
-    search: string
-  }
-  const initialValues: MyFormValues = { search: "" };
+  const { data, fetchPokemon, isLoading } = usePokemon();
+
+  const handleSearchSubmit = (search: string) => {
+    setIsOpen((isOpen) => !isOpen);
+    fetchPokemon(search);
+  };
 
   return (
-    <Container borderColor="gray.200" maxW="container.lg"  >
-      <Center h={"800px"}  >
-        <Flex>
-          <Box w={"500px"} >
-            <Formik
-              initialValues={initialValues}
-              onSubmit={(values, actions) => {
-                setTimeout(() => {
-                  navigate({ to: `/pokemon/${values.search}` })
-                  actions.setSubmitting(false);
-                }, 1000)
-              }}
-            >
-              {(props) => (
-                <Form >
-                  <Flex>
-                    <Field name="search" validate={validateSearch} >
-                      {({ field, form }: FieldProps) => (
-                        <FormControl isInvalid={!!form.errors.search} >
+    <Container maxW="container.lg">
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        position={"relative"}
+        minHeight={"85vh"}
+      >
+        <Box w="580px" position={"absolute"} top="45%">
+          <motion.div animate={isOpen ? "open" : "closed"} variants={variants}>
 
-                          <Input {...field} placeholder="search" />
-                          <FormErrorMessage>{form.errors.search?.toString()}</FormErrorMessage>
-                        </FormControl>
-                      )}
-                    </Field>
-                    <Button
-                      ml={4}
-                      py={2}
-                      colorScheme="teal" variant='outline' width='200px'
-                      isLoading={props.isSubmitting}
-                      type="submit"
-                    >
-                      let's find pokemon
-                    </Button>
-                  </Flex>
+            <Flex>
+              <Heading size="lg" color="gray" mr={2} >
+                Search your favorite Pokémon
+              </Heading>
+              <Popover >
+                <PopoverTrigger>
+                  <Button size={"sm"} variant={"outline"} ><Icon as={FiBookOpen} px={1} w={6} h={6} />info pokemon</Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                  <PopoverArrow />
+                  <PopoverCloseButton />
+                  <PopoverHeader>Confirmation!</PopoverHeader>
+                  <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
+                </PopoverContent>
+              </Popover>
+            </Flex>
 
-                </Form>
-              )}
-            </Formik>
-          </Box>
-        </Flex>
-      </Center>
+
+            <SearchForm onSubmit={handleSearchSubmit} />
+          </motion.div>
+        </Box>{" "}
+        <Center w={"700px"}>
+          {isLoading ? (
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="xl"
+            />
+          ) : (
+            data && <PokemonCard data={data} />
+          )}
+        </Center>
+      </Flex>
     </Container>
   );
 }
